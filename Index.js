9491a92b71447e6f0d6333efe79008ab2f075a4e
aryankakadiya81 <aryankakadiya81@gmail.com>
2024-09-25 11:25:13 +0530

    node Change

--- Index.js
+++ Index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+var cors = require('cors');
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
 const route = express.Router();
 const port = process.env.PORT || 5000;
 app.use('/v1', route);
@@ -38,7 +42,7 @@ route.post("/Mail", (req, res) => {
             console.log(err);
         }
 
-        console.log(info);
+        // console.log(info);
         res.status(200).send({ msg: "Your Data Added Successfully" ,info});
         // transporter.close();
     });
