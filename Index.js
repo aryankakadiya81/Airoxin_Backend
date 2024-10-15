0713ad2dcc1b8421c86d9df34f77e2b557f6bd0f
aryankakadiya81 <aryankakadiya81@gmail.com>
2024-10-15 17:23:05 +0530

    Country Add

--- Index.js
+++ Index.js
@@ -27,14 +27,22 @@ const transporter = nodemailer.createTransport({
 
 
 route.post("/Mail", (req, res) => {
-    let { Name, Email, Mobile, Sub, Txt } = req.body;
+    let { Name, Email, Country, CountryCode, CountryDielCode, Mobile, Sub, Txt } = req.body;
 
     let MailData = {
         from: 'aryan.airoxin@gmail.com', // sender address
         to: "info.airoxin@gmail.com", // list of receivers
         subject: Sub, // Subject line
         text: Txt,
-        html: `<h1>Email Data</h1><h3>Name:${Name}</h3><h3>Email:${Email}</h3><h3>Mobile:${Mobile}</h3><h3>Subject:${Sub}</h3><h3>Details:${Txt}</h3>` 
+        html: `<h1>Email Data</h1>
+        <h3>Name:${Name}</h3>
+        <h3>Email:${Email}</h3>
+        <h3>Country:${Country}</h3>
+        <h3>CountryCode:${CountryCode}</h3>
+        <h3>CountryDielCode:${CountryDielCode}</h3>
+        <h3>Mobile:${Mobile}</h3>
+        <h3>Subject:${Sub}</h3>
+        <h3>Details:${Txt}</h3>`
     }
 
     transporter.sendMail(MailData, function (err, info) {
@@ -43,7 +51,7 @@ route.post("/Mail", (req, res) => {
         }
 
         // console.log(info);
-        res.status(200).send({ msg: "Your Data Added Successfully" ,info});
+        res.status(200).send({ msg: "Your Data Added Successfully", info });
         // transporter.close();
     });
 })
